e843582955bd5abb3e1bf1c99d83518f02860696
chrysalis1215 <785851812@qq.com>
2021-03-23 17:13:45 +0800

    feat: 添加请求和mock方案

---
+++ mock/index.ts
@@ -0,0 +1,33 @@
+
+import { MockMethod } from 'vite-plugin-mock';
+const Mock = require('mockjs');
+export default [
+  {
+    url: '/api/get',
+    method: 'get',
+    response: ({ query }) => {
+      return {
+        code: 0,
+        data: {
+          name: 'vben',
+          list: Mock.mock({
+            'list|1-10': [{
+                'id|+1': 1
+            }]
+        })
+        },
+      };
+    },
+  },
+  {
+    url: '/api/post',
+    method: 'post',
+    timeout: 2000,
+    response: {
+      code: 0,
+      data: {
+        name: 'vben',
+      },
+    },
+  },
+] as MockMethod[];
--- src/router/index.js
+++ src/router/index.js
@@ -1,21 +1,21 @@
 import routeConfig from '@/config/routes.js';
 
 const getMenuRoutes = (list, parent) => {
-        if (!list) {
-            return [];
-        }
-        return list.map( item  => {
-            const { path = '', component } = item;
-            return {
-                path: path,
-                component: () => import(component),
-                children: getMenuRoutes(item.children, item),
-                meta: {
-                    title: item.title
-                }
+    if (!list) {
+        return [];
+    }
+    return list.map( item  => {
+        const { path = '', component } = item;
+        return {
+            path: path,
+            component: () => import(component),
+            children: getMenuRoutes(item.children, item),
+            meta: {
+                title: item.title
             }
-        }) 
-    };
+        }
+    }) 
+};
 
 const routes = [
     ...getMenuRoutes(routeConfig),
