04e2f55c6d4e72538ab396df86e9055f78dd1a9a
uyarn <uyarnchen@gmail.com>
2021-04-02 11:43:29 +0800

    chore: 处理codecc的告警

--- src/config/proxy.js
+++ src/config/proxy.js
@@ -1,13 +1,13 @@
 export default {
-    development: {
-      // 开发环境接口请求，后用于 proxy 代理配置
-    },
-    stage: {
-      // 测试环境 cdn 路径
-      // 测试环境接口地址
-    },
-    release: {
-      // 正式环境 cdn 路径
-     // 正式环境接口地址
-    }
-  }
+  development: {
+    // 开发环境接口请求，后用于 proxy 代理配置
+  },
+  stage: {
+    // 测试环境 cdn 路径
+    // 测试环境接口地址
+  },
+  release: {
+    // 正式环境 cdn 路径
+    // 正式环境接口地址
+  },
+};
