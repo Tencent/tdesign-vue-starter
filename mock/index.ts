c8196a8515c1e0cef87f6c0145bd71af08b552aa
James Liauw <jamesliauw@163.com>
2021-04-05 14:03:16 +0800

    feat: add mock

--- mock/index.ts
+++ mock/index.ts
@@ -9,16 +9,36 @@ export default [
       code: 0,
       data: {
         ...Mock.mock({
-          'list|1-100': [{
-            'index|+1': 1,
-            'name': '@ctitle(2,4)',
-            'updateTime': '@date("HH:mm:ss")',
-            'adminName': '@cname'
-        }]
-      }),
-    }
-  }),
-},
+          'list|1-100': [
+            {
+              'index|+1': 1,
+              name: '@ctitle(2,4)',
+              updateTime: '@date("HH:mm:ss")',
+              adminName: '@cname',
+            },
+          ],
+        }),
+      },
+    }),
+  },
+  {
+    url: '/api/detail-basic',
+    method: 'get',
+    response: () => ({
+      code: 0,
+      data: {
+        ...Mock.mock({
+          name: 'td_20023747',
+          loginType: 'Web',
+          currentRole: 'Admin',
+          rightsList: '通用权限',
+          userStatus: '启用',
+          language: '简体中文',
+          timeZone: '(GMT+08:00)中国时区—北京（Asia/Beijing）',
+        }),
+      },
+    }),
+  },
   {
     url: '/api/post',
     method: 'post',
