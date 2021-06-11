dc8e14c9e4330a66739c0741aa2166abb94403a1
uyarn <uyarnchen@gmail.com>
2021-06-11 14:31:21 +0800

    feat: 本地提供本地Mock和线上接口两种开发模式

--- globals.d.ts
+++ globals.d.ts
@@ -1,2 +1,8 @@
 // 通用声明
 declare type ClassName = { [className: string]: any } | ClassName[] | string;
+
+declare interface ImportMeta {
+  env: {
+    MODE: 'mock' | 'development' | 'test' | 'release';
+  };
+}
--- src/utils/request.js
+++ src/utils/request.js
@@ -3,7 +3,7 @@ import proxy from '../config/proxy';
 
 const env = import.meta.env.MODE || 'development';
 
-const { host } = proxy[env];
+const host = env === 'mock' ? '/' : proxy[env].host; // 如果是mock模式 就不配置host 会走本地Mock拦截
 
 const CODE = {
   LOGIN_TIMEOUT: 1000,
