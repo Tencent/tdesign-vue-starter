485bc9c520d6928424fe741e30808617449cfa68
PY <pengyue970715@gmail.com>
2021-12-08 17:25:06 +0800

    refactor: refactor login page (#7)
    * refactor: refactor login page
    
    * chore: complete github action
    
    * Chore/standard ts (#14)
    
    * feat: 添加规范说明
    
    * feat: 页面PAGE规范化
    
    * fix: 增加缺失的方法
    
    * feat: 更改DOCS说明
    
    * feat: 更改assets
    
    * feat: 更改tdesign-starter说明
    
    * feat: 去除饼图图例选择事件
    
    * feat: 去除pipe charts选中
    
    Co-authored-by: 1166544 <jamesliauw@163.com>
    
    * chore: repo optimize
    
    * feat: add ts
    
    Co-authored-by: pengYYYYY <pengyue970715@gmail.com>
    Co-authored-by: Uyarn <uyarnchen@gmail.com>
    Co-authored-by: 1166544 <jamesliauw@163.com>
    Co-authored-by: 1166544 <jamesliauw@163.com>

---
+++ src/pages/login/components/Login.vue
@@ -0,0 +1,147 @@
+<template>
+  <t-form
+    :class="['item-container', `login-${type}`]"
+    :data="formData"
+    ref="form"
+    :rules="FORM_RULES"
+    @submit="onSubmit"
+    labelWidth="0"
+  >
+    <template v-if="type == 'password'">
+      <t-form-item name="account">
+        <t-input v-model="formData.account" size="large" placeholder="请输入您的邮箱/手机号">
+          <template #prefix-icon>
+            <t-icon name="user" />
+          </template>
+        </t-input>
+      </t-form-item>
+
+      <t-form-item name="password">
+        <t-input
+          v-model="formData.password"
+          size="large"
+          :type="showPsw ? 'text' : 'password'"
+          clearable
+          placeholder="请输入登录密码"
+        >
+          <template #prefix-icon>
+            <t-icon name="lock-on" />
+          </template>
+          <template #suffix-icon>
+            <t-icon :name="showPsw ? 'browse' : 'browse-off'" @click="showPsw = !showPsw" />
+          </template>
+        </t-input>
+      </t-form-item>
+
+      <div class="check-container remember-pwd">
+        <t-checkbox>记住账号</t-checkbox>
+        <span class="tip">忘记账号？</span>
+      </div>
+    </template>
+
+    <!-- 扫码登陆 -->
+    <template v-else-if="type == 'qrcode'">
+      <div class="tip-container">
+        <span class="tip1">请使用微信扫一扫登录</span>
+        <span class="tip2 refresh">刷新 <t-icon name="refresh" color="#0052D9" /> </span>
+      </div>
+      <qrcode-vue value="" :size="192" level="H" />
+    </template>
+
+    <!-- 手机号登陆 -->
+    <template v-else>
+      <t-form-item name="phone">
+        <t-input v-model="formData.phone" size="large" placeholder="请输入您的手机号">
+          <template #prefix-icon>
+            <t-icon name="user" />
+          </template>
+        </t-input>
+      </t-form-item>
+
+      <t-form-item class="verification-code" name="verifyCode">
+        <t-input v-model="formData.verifyCode" size="large" placeholder="请输入验证码" />
+        <t-button variant="outline" :disabled="countDown > 0" @click="handleCounter">
+          {{ countDown == 0 ? '发送验证码' : `${countDown}秒后可重发` }}
+        </t-button>
+      </t-form-item>
+    </template>
+
+    <t-form-item v-if="type !== 'qrcode'">
+      <t-button block size="large" type="submit"> 登录 </t-button>
+    </t-form-item>
+
+    <div class="switch-container">
+      <span class="tip" v-if="type !== 'password'" @click="switchType('password')">使用账号密码登录</span>
+      <span class="tip" v-if="type !== 'qrcode'" @click="switchType('qrcode')">使用微信扫码登录</span>
+      <span class="tip" v-if="type !== 'phone'" @click="switchType('phone')">使用手机号登录</span>
+    </div>
+  </t-form>
+</template>
+<script lang="ts">
+import Vue from 'vue';
+import QrcodeVue from 'qrcode.vue';
+import { passwordValidator } from '../helper';
+
+const INITIAL_DATA = {
+  phone: '',
+  account: '',
+  password: '',
+  verifyCode: '',
+  checked: false,
+};
+
+const FORM_RULES = {
+  phone: [{ required: true, message: '手机号必填', type: 'error' }],
+  account: [{ required: true, message: '账号必填', type: 'error' }],
+  password: [{ required: true, message: '密码必填', type: 'error' }, { validator: passwordValidator }],
+  verifyCode: [{ required: true, message: '验证码必填', type: 'error' }],
+};
+/** 高级详情 */
+export default Vue.extend({
+  name: 'Login',
+  components: {
+    QrcodeVue,
+  },
+  data() {
+    return {
+      FORM_RULES,
+      type: 'password',
+      formData: { ...INITIAL_DATA },
+      showPsw: false,
+      countDown: 0,
+      intervalTimer: null,
+    };
+  },
+  beforeDestroy() {
+    clearInterval(this.intervalTimer);
+  },
+  methods: {
+    switchType(val) {
+      this.type = val;
+      this.$refs.form.reset();
+    },
+    onSubmit({ validateResult }) {
+      if (validateResult === true) {
+        this.$store.commit('user/SET_USER_INFO', this.userInfo);
+
+        this.$message.success('登录成功');
+
+        this.$router.push({
+          path: '/',
+        });
+      }
+    },
+    handleCounter() {
+      this.countDown = 60;
+      this.intervalTimer = setInterval(() => {
+        if (this.countDown > 0) {
+          this.countDown -= 1;
+        } else {
+          clearInterval(this.intervalTimer);
+          this.countDown = 0;
+        }
+      }, 1000);
+    },
+  },
+});
+</script>
---
+++ src/pages/login/components/Register.vue
@@ -0,0 +1,155 @@
+<template>
+  <t-form
+    ref="form"
+    :class="['item-container', `register-${type}`]"
+    :data="formData"
+    :rules="FORM_RULES"
+    label-width="0"
+    @submit="onSubmit"
+  >
+    <template v-if="type == 'phone'">
+      <t-form-item name="phone">
+        <t-input
+          v-model="formData.phone"
+          :maxlength="11"
+          style="width: 400px"
+          size="large"
+          placeholder="请输入您的手机号"
+        >
+          <template #prefix-icon>
+            <t-icon name="user" />
+          </template>
+        </t-input>
+      </t-form-item>
+    </template>
+
+    <template v-if="type == 'email'">
+      <t-form-item name="email">
+        <t-select
+          v-model="formData.email"
+          placeholder="请输入您的邮箱"
+          filterable
+          size="large"
+          :empty="''"
+          :options="emailOptions"
+          :on-search="remoteMethod"
+        >
+          <template #t-input>
+            <t-icon name="lock-on" />
+          </template>
+        </t-select>
+      </t-form-item>
+    </template>
+
+    <t-form-item name="password">
+      <t-input
+        v-model="formData.password"
+        size="large"
+        :type="showPsw ? 'text' : 'password'"
+        clearable
+        placeholder="请输入登录密码"
+      >
+        <template #prefix-icon>
+          <t-icon name="lock-on" />
+        </template>
+        <template #suffix-icon>
+          <t-icon :name="showPsw ? 'browse' : 'browse-off'" @click="showPsw = !showPsw" />
+        </template>
+      </t-input>
+    </t-form-item>
+
+    <template v-if="type == 'phone'">
+      <t-form-item class="verification-code" name="verifyCode">
+        <t-input v-model="formData.verifyCode" size="large" placeholder="请输入验证码" />
+        <t-button variant="outline" :disabled="countDown > 0" @click="handleCounter">
+          {{ countDown == 0 ? '发送验证码' : `${countDown}秒后可重发` }}
+        </t-button>
+      </t-form-item>
+    </template>
+
+    <t-form-item class="check-container" name="checked">
+      <t-checkbox v-model="formData.checked">我已阅读并同意 </t-checkbox> <span>TDesign服务协议</span> 和
+      <span>TDesign 隐私声明</span>
+    </t-form-item>
+
+    <t-form-item>
+      <t-button block size="large" type="submit"> 注册 </t-button>
+    </t-form-item>
+
+    <div class="switch-container">
+      <span class="tip" @click="switchType(type == 'phone' ? 'email' : 'phone')">{{
+        type == 'phone' ? '使用邮箱注册' : '使用手机号注册'
+      }}</span>
+    </div>
+  </t-form>
+</template>
+<script lang="ts">
+import Vue from 'vue';
+import { passwordValidator, getEmails } from '../helper';
+
+const INITIAL_DATA = {
+  phone: '',
+  email: '',
+  password: '',
+  verifyCode: '',
+  checked: false,
+};
+
+const FORM_RULES = {
+  phone: [{ required: true, message: '手机号必填', type: 'error' }],
+  email: [{ required: true, email: true, message: '邮箱必填', type: 'error' }],
+  password: [{ required: true, message: '密码必填', type: 'error' }, { validator: passwordValidator }],
+  verifyCode: [{ required: true, message: '验证码必填', type: 'error' }],
+};
+
+/** 高级详情 */
+export default Vue.extend({
+  name: 'Register',
+  data() {
+    return {
+      FORM_RULES,
+      type: 'phone',
+      emailOptions: [],
+      formData: { ...INITIAL_DATA },
+      showPsw: false,
+      countDown: 0,
+      intervalTimer: null,
+    };
+  },
+  beforeDestroy() {
+    clearInterval(this.intervalTimer);
+  },
+  methods: {
+    onSubmit({ validateResult }) {
+      if (validateResult === true) {
+        if (!this.formData.checked) {
+          this.$message.error('请同意TDesign服务协议和TDesign 隐私声明');
+          return;
+        }
+        this.$message.success('注册成功');
+        this.$emit('registerSuccess');
+      }
+    },
+    remoteMethod(search) {
+      if (search && search.indexOf('@') === -1) {
+        this.emailOptions = getEmails(search);
+      }
+    },
+    switchType(val) {
+      this.$refs.form.reset();
+      this.type = val;
+    },
+    handleCounter() {
+      this.countDown = 60;
+      this.intervalTimer = setInterval(() => {
+        if (this.countDown > 0) {
+          this.countDown -= 1;
+        } else {
+          clearInterval(this.intervalTimer);
+          this.countDown = 0;
+        }
+      }, 1000);
+    },
+  },
+});
+</script>
