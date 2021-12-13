562344dd44441b46d671edae626d907cbffad7a9
PY <pengyue970715@gmail.com>
2021-12-13 17:26:12 +0800

    chore/spell (#21)
    * chore: spell fix and user index change theme
    
    * chore: global config upgrade
    
    * chore: remove useless code
    
    * fix: color fix

--- stylelint.config.js
+++ stylelint.config.js
@@ -1,10 +1,5 @@
 module.exports = {
   defaultSeverity: 'error',
-  extends: ['stylelint-config-airbnb'],
-  plugins: ['stylelint-scss'],
-  rules: {
-    'max-nesting-depth': 10,
-    'string-quotes': 'single',
-    'declaration-property-value-disallowed-list': { '/^border/': ['none'] },
-  },
+  extends: ['stylelint-config-prettier'],
+  plugins: ['stylelint-less'],
 };
