a10b822865d75358fe88fca9b3efb77aaaf9d1cc
pengYYY <pengyue970715@gmail.com>
2021-12-22 11:46:42 +0800

    chore: user page update

--- src/pages/user/index.vue
+++ src/pages/user/index.vue
@@ -1,104 +1,102 @@
 <template>
-  <div>
-    <t-row :gutter="16">
-      <t-col :flex="3">
-        <div class="user-left-greeting">
-          <div>
-            Hi，Image
-            <span class="regular"> 下午好，今天是你加入鹅厂的第 100 天～</span>
-          </div>
-          <img src="@/assets/assets-tencent-logo.png" class="logo" />
+  <t-row :gutter="[24, 24]">
+    <t-col :flex="3">
+      <div class="user-left-greeting">
+        <div>
+          Hi，Image
+          <span class="regular"> 下午好，今天是你加入鹅厂的第 100 天～</span>
         </div>
+        <img src="@/assets/assets-tencent-logo.png" class="logo" />
+      </div>
 
-        <card class="user-info-list" size="small" title="个人信息">
-          <template #option>
-            <t-button theme="default" shape="square" variant="text">
-              <t-icon name="edit" size="18" />
-            </t-button>
-          </template>
-          <t-row class="content" justify="space-between">
-            <t-col v-for="(item, index) in USER_INFO_LIST" :key="index" class="contract" :span="item.span || 3">
-              <div class="contract-title">
-                {{ item.title }}
-              </div>
-              <div class="contract-detail">
-                {{ item.content }}
-              </div>
-            </t-col>
-          </t-row>
-        </card>
+      <card class="user-info-list" size="small" title="个人信息">
+        <template #option>
+          <t-button theme="default" shape="square" variant="text">
+            <t-icon name="edit" size="18" />
+          </t-button>
+        </template>
+        <t-row class="content" justify="space-between">
+          <t-col v-for="(item, index) in USER_INFO_LIST" :key="index" class="contract" :span="item.span || 3">
+            <div class="contract-title">
+              {{ item.title }}
+            </div>
+            <div class="contract-detail">
+              {{ item.content }}
+            </div>
+          </t-col>
+        </t-row>
+      </card>
 
-        <card class="content-container">
-          <t-tabs value="second">
-            <t-tab-panel value="first" label="内容列表">
-              <p>内容列表</p>
-            </t-tab-panel>
-            <t-tab-panel value="second" label="内容列表">
-              <card class="card-padding-no" title="主页访问数据" describe="（次）">
-                <template #options>
-                  <t-date-picker
-                    class="card-date-picker-container"
-                    :default-value="LAST_7_DAYS"
-                    theme="primary"
-                    mode="date"
-                    range
-                    @change="onLineChange"
-                  />
-                </template>
-                <div id="lineContainer" style="width: 100%; height: 330px" />
-              </card>
-            </t-tab-panel>
-            <t-tab-panel value="third" label="内容列表">
-              <p>内容列表</p>
-            </t-tab-panel>
-          </t-tabs>
-        </card>
-      </t-col>
+      <card class="content-container">
+        <t-tabs value="second">
+          <t-tab-panel value="first" label="内容列表">
+            <p>内容列表</p>
+          </t-tab-panel>
+          <t-tab-panel value="second" label="内容列表">
+            <card class="card-padding-no" title="主页访问数据" describe="（次）">
+              <template #options>
+                <t-date-picker
+                  class="card-date-picker-container"
+                  :default-value="LAST_7_DAYS"
+                  theme="primary"
+                  mode="date"
+                  range
+                  @change="onLineChange"
+                />
+              </template>
+              <div id="lineContainer" style="width: 100%; height: 330px" />
+            </card>
+          </t-tab-panel>
+          <t-tab-panel value="third" label="内容列表">
+            <p>内容列表</p>
+          </t-tab-panel>
+        </t-tabs>
+      </card>
+    </t-col>
 
-      <t-col :flex="1">
-        <card class="user-intro">
-          <t-avatar size="90px">T</t-avatar>
-          <div class="name">My Account</div>
-          <div class="position">XXG 港澳业务拓展组员工 直客销售</div>
-        </card>
+    <t-col :flex="1">
+      <card class="user-intro">
+        <t-avatar size="90px">T</t-avatar>
+        <div class="name">My Account</div>
+        <div class="position">XXG 港澳业务拓展组员工 直客销售</div>
+      </card>
 
-        <card title="团队成员" class="user-team" size="small">
-          <template #option>
-            <t-button theme="default" shape="square" variant="text">
-              <t-icon name="edit" size="18" />
-            </t-button>
-          </template>
-          <t-list :split="false">
-            <t-list-item v-for="(item, index) in TEAM_MEMBERS" :key="index">
-              <t-list-item-meta :image="item.avatar" :title="item.title" :description="item.description" />
-            </t-list-item>
-          </t-list>
-        </card>
+      <card title="团队成员" class="user-team" size="small">
+        <template #option>
+          <t-button theme="default" shape="square" variant="text">
+            <t-icon name="edit" size="18" />
+          </t-button>
+        </template>
+        <t-list :split="false">
+          <t-list-item v-for="(item, index) in TEAM_MEMBERS" :key="index">
+            <t-list-item-meta :image="item.avatar" :title="item.title" :description="item.description" />
+          </t-list-item>
+        </t-list>
+      </card>
 
-        <card title="服务产品" class="product-container" size="small">
-          <template #option>
-            <t-button theme="default" shape="square" variant="text">
-              <t-icon name="edit" size="18" />
-            </t-button>
-          </template>
-          <t-row class="content" :getters="16">
-            <t-col :span="3">
-              <product-a-icon />
-            </t-col>
-            <t-col :span="3">
-              <product-b-icon />
-            </t-col>
-            <t-col :span="3">
-              <product-c-icon />
-            </t-col>
-            <t-col :span="3">
-              <product-d-icon />
-            </t-col>
-          </t-row>
-        </card>
-      </t-col>
-    </t-row>
-  </div>
+      <card title="服务产品" class="product-container" size="small">
+        <template #option>
+          <t-button theme="default" shape="square" variant="text">
+            <t-icon name="edit" size="18" />
+          </t-button>
+        </template>
+        <t-row class="content" :getters="16">
+          <t-col :span="3">
+            <product-a-icon />
+          </t-col>
+          <t-col :span="3">
+            <product-b-icon />
+          </t-col>
+          <t-col :span="3">
+            <product-c-icon />
+          </t-col>
+          <t-col :span="3">
+            <product-d-icon />
+          </t-col>
+        </t-row>
+      </card>
+    </t-col>
+  </t-row>
 </template>
 <script>
 import { prefix } from '@/config/global';
