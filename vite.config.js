76e677b916772cd878dc6413fd333c5eb7ee94da
Uyarn <uyarnchen@gmail.com>
2022-03-21 15:42:55 +0800

    fix: vite config base url

--- vite.config.js
+++ vite.config.js
@@ -1,57 +1,60 @@
-import { defineConfig } from 'vite';
+import { loadEnv } from 'vite';
 import { viteMockServe } from 'vite-plugin-mock';
 import { createVuePlugin } from 'vite-plugin-vue2';
 import { createSvgPlugin } from 'vite-plugin-vue2-svg';
 
 import path from 'path';
 
-// 如有引用.env 文件，可通过下面的方法拿到变量，类似于 process.env
-// import.meta.env.VITE_SOME_KEY
+const CWD = process.cwd();
 
-export default defineConfig({
-  base: './',
-  resolve: {
-    alias: {
-      '~': path.resolve(__dirname, './'),
-      '@': path.resolve(__dirname, './src'),
+export default ({ mode }) => {
+  const { VITE_BASE_URL } = loadEnv(mode, CWD);
+
+  return {
+    base: VITE_BASE_URL,
+    resolve: {
+      alias: {
+        '~': path.resolve(__dirname, './'),
+        '@': path.resolve(__dirname, './src'),
+      },
     },
-  },
 
-  css: {
-    preprocessorOptions: {
-      less: {
-        modifyVars: {
-          // 如需自定义组件其他 token, 在此处配置
+    css: {
+      preprocessorOptions: {
+        less: {
+          modifyVars: {
+            // 如需自定义组件其他 token, 在此处配置
+          },
         },
       },
     },
-  },
 
-  plugins: [
-    createVuePlugin({
-      jsx: true,
-    }),
-    viteMockServe({
-      mockPath: 'mock',
-      localEnabled: true,
-    }),
-    createSvgPlugin(),
-  ],
+    plugins: [
+      createVuePlugin({
+        jsx: true,
+      }),
+      viteMockServe({
+        mockPath: 'mock',
+        localEnabled: true,
+      }),
+      createSvgPlugin(),
+    ],
 
-  build: {
-    cssCodeSplit: false,
-  },
+    build: {
+      cssCodeSplit: false,
+    },
 
-  server: {
-    host: '0.0.0.0',
-    port: 3001,
-    proxy: {
-      '/api': {
-        // 用于开发环境下的转发请求
-        // 更多请参考：https://vitejs.dev/config/#server-proxy
-        target: 'https://service-exndqyuk-1257786608.gz.apigw.tencentcs.com',
-        changeOrigin: true,
+    server: {
+      host: '0.0.0.0',
+      port: 3001,
+      proxy: {
+        '/api': {
+          // 用于开发环境下的转发请求
+          // 更多请参考：https://vitejs.dev/config/#server-proxy
+          target: 'https://service-exndqyuk-1257786608.gz.apigw.tencentcs.com',
+          changeOrigin: true,
+        },
       },
     },
-  },
-});
+  };
+};
